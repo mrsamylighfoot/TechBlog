30565f00fd2efd9466b1734040828a42c1cb47dd
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-28 18:15:20 -0400

    updating routes and server

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
-const session = require("express-session");
+const path = require("path");
 const exphbs = require("express-handlebars");
-// const helpers = require('./utils/helpers');
 
-const app = express();
-
+const routes = require("./controllers");
 const models = require("./models");
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-const sequelize = require("./config/connection");
+const hbs = exphbs.create({ helpers });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+  });
+});
+
+const session = require("express-session");
+// const helpers = require('./utils/helpers');
+
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
@@ -26,18 +41,5 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-
 // app.engine('handlebars', hbs.engine);
 // app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(routes);
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
