87213d7f28a655ac46d57657bab1e006c29ad82a
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-29 08:39:26 -0400

    more route updates

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,20 @@
+const { json } = require("express/lib/response");
+
 const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/sign-up", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    res.status(200).json();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const { User } = require("../models");
 
 router.get("/", async (req, res) => {
-  res.render('homepage');
+  try {
+    const userData = await User.findAll();
+    const serializedUser = userData.map(user => user.get({ plain: true }));
+    console.log(serializedUser);
+    res.render("homepage", {
+        serializedUser,
+    });
+  } catch (err) {}
 });
 
 module.exports = router();
