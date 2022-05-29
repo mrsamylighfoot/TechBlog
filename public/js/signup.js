349933284ce21f43601977b97fdc3402c1ae665d
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-29 13:33:38 -0400

    updated signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,25 @@
-const signupFormHandler = async (event) => {
+const signupFormHandler = async function (event) {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const usernameEl = document.querySelector("#username-input-signup");
+  const passwordEl = document.querySelector("#password-input-signup");
 
-  if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to sign up");
   }
 };
 
 document
-  .querySelector(".signup-form")
+  .querySelector("#signup-form")
   .addEventListener("submit", signupFormHandler);
