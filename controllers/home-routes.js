b58e1bc016591e26a1d47535c09b38fc3d867a4a
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-29 09:56:57 -0400

    updating routers for api

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,72 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const res = require("express/lib/response");
+const { User, Post, Comment } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const userData = await User.findAll();
-    const serializedUser = userData.map(user => user.get({ plain: true }));
-    console.log(serializedUser);
-    res.render("homepage", {
-        serializedUser,
+    const postData = await Post.findAll({
+      include: [User],
     });
-  } catch (err) {}
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("all-posts", { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
 });
 
 module.exports = router();
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const userData = await User.findAll();
+//     const serializedUser = userData.map(user => user.get({ plain: true }));
+//     console.log(serializedUser);
+//     res.render("homepage", {
+//         serializedUser,
+//     });
+//   } catch (err) {}
+// });
