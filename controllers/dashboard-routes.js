f16544cd9c5a7d4cbd40fb2f7ea3bd6ddfc36e93
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-29 09:27:58 -0400

    dashboard routes updated using withAuth

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,4 +1,4 @@
-const req = require("express").Router();
+const router = require("express").Router();
 const { Post } = require("../models");
 const withAuth = require("../utils/auth");
 const router = require("./api/user-routes");
@@ -17,6 +17,31 @@ router.get("/", withAuth, async (req, res) => {
       layout: "dashboard",
       posts,
     });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    layout: "dashboard",
+  });
+});
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
     res.redirect("login");
   }
